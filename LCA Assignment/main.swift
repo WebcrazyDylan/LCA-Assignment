21fb20e98a4a4bc46878a9e73d0ea4ac396b36ff
WebcrazyDylan <webcrazy@naver.com>
2021-08-31 16:28:57 -0700

    Update Code

--- LCA Assignment/main.swift
+++ LCA Assignment/main.swift
@@ -7,60 +7,108 @@
 
 import Foundation
 
-func lca() {
-  let n = Int(readLine()!)!
-  var adj = [[Int]](repeating: [], count: n + 1)
-  var depth = [Int](repeating: 0, count: n + 1)
-  var parent = [Int](repeating: 0, count: n + 1)
-  var check = [Bool](repeating: false, count: n + 1)
-  
-  for _ in 0..<n-1 {
-    let edge = readLine()!.split(separator: " ").map { Int($0)! }
-    let u = edge[0]
-    let v = edge[1]
-    adj[u].append(v)
-    adj[v].append(u)
-  }
-  
-  // preprocessing
-  depth[1] = 0
-  check[1] = true
-  let q = Queue<Int>()
-  q.enqueue(item: 1)
-  
-  while !q.isEmpty() {
-    let u = q.dequeue()!
-    for v in adj[u] {
-      if !check[v] {
-        depth[v] = depth[u] + 1
-        parent[v] = u
-        check[v] = true
-        q.enqueue(item: v)
-      }
+var alreadyExist = [Int:Bool]()
+var children: [[Int]] = []
+
+func lowestCommonAncestor() {
+    print("Please input vertices for lowestCommonAncestor")
+    
+    let preN = readLine()!.split(separator: " ").map { Int($0) }
+    let n = preN[0]!
+    children = Array(repeating: Array(repeating: 0, count: 0), count: n + 1)
+    alreadyExist[1] = true
+    for i in 2...n {
+        alreadyExist[i] = false
     }
-  }
-  
-  var m = Int(readLine()!)!
-  while m > 0 {
-    let nodes = readLine()!.split(separator: " ").map { Int($0)! }
-    var x = nodes[0]
-    var y = nodes[1]
-    if depth[x] < depth[y] {
-      swap(&x, &y)
+    
+    for _ in 0..<n - 1 {
+        
+        let nodeInfo = readLine()!.split(separator: " ").map { Int($0) }
+        
+        if alreadyExist[nodeInfo[0]!] == false {
+            children[nodeInfo[1]!].append(nodeInfo[0]!)
+            alreadyExist[nodeInfo[0]!] = true
+        }
+        if alreadyExist[nodeInfo[1]!] == false {
+            children[nodeInfo[0]!].append(nodeInfo[1]!)
+            alreadyExist[nodeInfo[1]!] = true
+        }
     }
-    while depth[x] != depth[y] {
-      x = parent[x]
+    
+    let preM = readLine()!.split(separator: " ").map { Int($0) }
+    let m = preM[0]!
+    
+    for _ in 0..<m {
+        let nodeInfo = readLine()!.split(separator: " ").map { Int($0) }
+        let p = nodeInfo[0]!
+        let q = nodeInfo[1]!
+        let LCA = lcaHelper(root: 1, p: p, q: q)!
+        print("Lowest Common Ancestor of \(p) and \(q) is \(LCA).")
     }
-    while x != y {
-      x = parent[x]
-      y = parent[y]
-    }
-    print(x)
-    m -= 1
-  }
 }
 
 
+func lcaHelper(root: Int, p: Int, q: Int) -> Int? {
+    
+    if root == p || root == q {
+        // the root is the LCA
+        return root
+    }
+    if children[root].isEmpty {
+        // this root node is the dead end
+        return nil
+    }
+    var count = 0
+    var onlyFoundOne = 0
+    for i in children[root] {
+        if lcaHelper(root: i, p: p, q: q) != nil {
+            onlyFoundOne = lcaHelper(root: i, p: p, q: q)!
+            count += 1
+        }
+    }
+    if count == 2 {
+        // the root is the LCA
+        return root
+    } else if count == 1 {
+        // LCA exists higher than this
+        return onlyFoundOne
+    } else {
+        // nothing below this root node
+        return nil
+    }
+}
 
-lca()
+lowestCommonAncestor()
 
+/*
+ Sample Input1
+ 15
+ 1 2
+ 1 3
+ 2 4
+ 3 7
+ 6 2
+ 3 8
+ 4 9
+ 2 5
+ 5 11
+ 7 13
+ 10 4
+ 11 15
+ 12 5
+ 14 7
+ 6
+ 6 11
+ 10 9
+ 2 6
+ 7 6
+ 8 13
+ 8 15
+ Sample Output1
+ 2
+ 4
+ 2
+ 1
+ 3
+ 1
+ */
